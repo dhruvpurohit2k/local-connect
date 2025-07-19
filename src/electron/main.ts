a1623c306bd6ecacbf67ba5719258e6efcc05b60
dhruvpurohit2k <purohitdhruv00@gmail.com>
2025-07-19 19:58:49 +0530

    Initial Commit

---
+++ src/electron/main.ts
@@ -0,0 +1,11 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { isDev } from "./util.js";
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({ width: 1280, height: 720 });
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+  }
+});
